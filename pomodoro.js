ed0e38e412a5d6d431b905a882cfd03f83ba1c3e
Chris <mail@christopherparsons.me>
2017-05-04 13:58:34 +0100

    Finish Time
    Finish Time is now calculated and pushed onto the page.
    Remaining time is now calculated and pushed onto the page.

--- pomodoro.js
+++ pomodoro.js
@@ -1,13 +1,33 @@
 $( document ).ready(function() {
   
+  var minutes = 25;
+  
   $("#start").click(function endTime () {
-    var today = new Date();
-    var time = today.getHours() + ':' + today.getMinutes() + ':' + today.getSeconds();
+    setInterval(function() {
+      var now = new Date();
+      var time = now.getHours() + ':' + now.getMinutes() + ':' + now.getSeconds();
+
+      var hourDiff = (later - now)/60000;
+      console.log(hourDiff);
+    
+      $("#remaining").text(hourDiff + " remaining");
+      
+    }, 1000);
+    
+
+    
+    var start = new Date();
+    var time = start.getHours() + ':' + start.getMinutes() + ':' + start.getSeconds();
     console.log (time);
 
-    var today25 = new Date(today.getTime() + 25*60000);
-    var timeLimit = today25.getHours() + ':' + today25.getMinutes() + ':' + today.getSeconds();
+    var later = new Date(start.getTime() + 25*60000);
+    var timeLimit = later.getHours() + ':' + later.getMinutes() + ':' + later.getSeconds();
     console.log (timeLimit);
+    
+    $('<p>Finish Time: ' + timeLimit + '</p>').appendTo('#future');
+    
+
+    
     
   });
   
